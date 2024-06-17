fd0e286c8174a62079bce541097613dd29609756
Neirris <nepkka@gmail.com>
2024-06-17 11:07:14 +0500

    add static files support

--- server/server.mjs
+++ server/server.mjs
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { Server } from 'socket.io'
 import http from 'http'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { initializeDatabase } from './data/database.js'
 import { seedDatabase } from './data/seed.js'
 import authRouter from './routes/auth.js'
@@ -20,11 +22,13 @@ import adminRouter from './routes/admin.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    // eslint-disable-next-line no-undef
     origin: process.env.API_BASE_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
@@ -34,19 +38,31 @@ const io = new Server(server, {
 app.use(bodyParser.json({ limit: '25mb' }))
 app.use(bodyParser.urlencoded({ limit: '25mb', extended: true }))
 app.use(cookieParser())
-// eslint-disable-next-line no-undef
 app.use(cors({ origin: process.env.API_BASE_URL, credentials: true }))
 
 initializeDatabase()
 seedDatabase()
 
-app.use(authRouter)
-app.use(profileRouter)
-app.use(usersRouter)
-app.use(chatsRouter(io))
-app.use(eventsRouter(io))
-app.use(notificationsRouter)
+app.use('/auth', authRouter)
+app.use('/profile', profileRouter)
+app.use('/users', usersRouter)
+app.use('/chats', chatsRouter(io))
+app.use('/events', eventsRouter(io))
+app.use('/notifications', notificationsRouter)
 app.use('/admin', adminRouter)
+
+// Обслуживание статических файлов из dist
+app.use(express.static(path.join(__dirname, '../dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'))
+})
+
+// Обработка ошибок
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 // Подключение к WebSocket
 io.on('connection', (socket) => {
@@ -57,7 +73,6 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {})
 })
 
-// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`)
