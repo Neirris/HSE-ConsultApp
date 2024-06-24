516fed85f3c35b878c0bf5d47aa193e4a5da7e78
Neirris <nepkka@gmail.com>
2024-06-25 04:05:27 +0500

    add redirect

--- src/views/student/StudentMain.vue
+++ src/views/student/StudentMain.vue
@@ -1,3 +1,27 @@
 <template>
     <div></div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const router = useRouter();
+
+const fetchProfileData = async () => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/profile-data`, { withCredentials: true });
+        const userId = response.data.userId;
+        router.push(`/users/id/${userId}`);
+    } catch (error) {
+        console.error('Не удалось загрузить данные профиля:', error);
+    }
+};
+
+onMounted(() => {
+    fetchProfileData();
+});
+</script>
