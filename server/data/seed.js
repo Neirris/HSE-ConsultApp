ca1192ff0069eae363cb8607ee3ea4871add2914
Neirris <nepkka@gmail.com>
2024-06-25 03:55:02 +0500

    add optimization

--- server/data/seed.js
+++ server/data/seed.js
@@ -10,86 +10,114 @@ const __dirname = path.dirname(__filename)
 const defaultProfileImagePath = path.join(__dirname, 'assets', 'icons', 'DefaultPFP.png')
 const defaultProfileImage = fs.readFileSync(defaultProfileImagePath, { encoding: 'base64' })
 
+const createPermanentToken = (email, password) => {
+  const firstHash = CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex)
+  const combined = email + firstHash
+  return CryptoJS.SHA256(combined).toString(CryptoJS.enc.Hex)
+}
+
 export const seedDatabase = () => {
-  db.run(`
-    INSERT OR IGNORE INTO sections (name)
-    VALUES ('default')
-  `)
+  const educationPrograms = [
+    'Программная инженерия',
+    'Бизнес информатика',
+    'Разработка информационных систем'
+  ]
 
-  const adminPassword = '123321!!aA'
-  const teacherPassword = '123321!!aA'
-  const studentPassword = '123321!!aA'
-  const hashedAdminPassword = CryptoJS.SHA256(adminPassword).toString(CryptoJS.enc.Hex)
-  const hashedTeacherPassword = CryptoJS.SHA256(teacherPassword).toString(CryptoJS.enc.Hex)
-  const hashedStudentPassword = CryptoJS.SHA256(studentPassword).toString(CryptoJS.enc.Hex)
+  const sections = [
+    { name: 'default', program: null },
+    { name: 'ПИ-21-1', program: 'Программная инженерия' },
+    { name: 'ПИ-21-2', program: 'Программная инженерия' },
+    { name: 'ПИ-21-3', program: 'Программная инженерия' },
+    { name: 'БИ-21-1', program: 'Бизнес информатика' },
+    { name: 'БИ-21-2', program: 'Бизнес информатика' },
+    { name: 'БИ-21-3', program: 'Бизнес информатика' },
+    { name: 'РИС-22-1', program: 'Разработка информационных систем' },
+    { name: 'РИС-22-2', program: 'Разработка информационных систем' },
+    { name: 'РИС-22-3', program: 'Разработка информационных систем' }
+  ]
 
-  const adminToken = createPermanentToken('admin@mail.ru', hashedAdminPassword)
-  const teacherToken = createPermanentToken('teacher@mail.ru', hashedTeacherPassword)
-  const studentToken = createPermanentToken('student@mail.ru', hashedStudentPassword)
+  const users = [
+    { email: 'admin@mail.ru', password: '123321!!aA', accountType: 'admin', fullName: 'Админ' },
+    {
+      email: 'teacher@mail.ru',
+      password: '123321!!aA',
+      accountType: 'teacher',
+      fullName: 'Учитель'
+    },
+    {
+      email: 'student@mail.ru',
+      password: '123321!!aA',
+      accountType: 'student',
+      fullName: 'Студент'
+    }
+  ]
 
   db.serialize(() => {
-    // Отключение триггера
+    db.run(
+      `
+      INSERT OR IGNORE INTO educationPrograms (name)
+      VALUES ${educationPrograms.map(() => '(?)').join(', ')}
+    `,
+      educationPrograms
+    )
+
+    sections.forEach((section) => {
+      db.run(
+        `
+        INSERT OR IGNORE INTO sections (name, educationProgramId)
+        VALUES (?, (SELECT id FROM educationPrograms WHERE name = ?))
+      `,
+        [section.name, section.program]
+      )
+    })
+
+    users.forEach((user) => {
+      user.hashedPassword = CryptoJS.SHA256(user.password).toString(CryptoJS.enc.Hex)
+      user.token = createPermanentToken(user.email, user.hashedPassword)
+    })
+
+    // выключение триггера
     db.run(`DROP TRIGGER IF EXISTS set_default_section`)
 
     db.run(
       `
       INSERT OR IGNORE INTO users (email, password, accountType, token)
-      VALUES ('admin@mail.ru', ?, 'admin', ?),
-             ('teacher@mail.ru', ?, 'teacher', ?),
-             ('student@mail.ru', ?, 'student', ?)
+      VALUES ${users.map(() => '(?, ?, ?, ?)').join(', ')}
     `,
-      [
-        hashedAdminPassword,
-        adminToken,
-        hashedTeacherPassword,
-        teacherToken,
-        hashedStudentPassword,
-        studentToken
-      ],
+      users.flatMap((user) => [user.email, user.hashedPassword, user.accountType, user.token]),
       function (err) {
         if (err) {
           return console.error(err.message)
         }
 
-        db.get(
-          `
-        SELECT id FROM sections WHERE name = 'default'
-      `,
-          (err, section) => {
-            if (err) {
-              return console.error(err.message)
-            }
-            const defaultSectionId = section.id
-
-            const adminId = this.lastID - 2
-            const teacherId = this.lastID - 1
-            const studentId = this.lastID
-
-            db.run(
-              `
-          INSERT OR IGNORE INTO profiles (userId, fullName, description, profileImage, sectionId)
-          VALUES (?, 'Админ', '', ?, ?),
-                 (?, 'Учитель', '', ?, ?),
-                 (?, 'Студент', '', ?, ?)
-        `,
-              [
-                adminId,
-                defaultProfileImage,
-                defaultSectionId,
-                teacherId,
-                defaultProfileImage,
-                defaultSectionId,
-                studentId,
-                defaultProfileImage,
-                defaultSectionId
-              ]
-            )
+        db.get(`SELECT id FROM sections WHERE name = 'default'`, (err, section) => {
+          if (err) {
+            return console.error(err.message)
           }
-        )
+
+          const defaultSectionId = section.id
+
+          const profileValues = users
+            .map((user, index) => [
+              this.lastID - users.length + index + 1, // userId
+              user.fullName,
+              '',
+              defaultProfileImage,
+              defaultSectionId
+            ])
+            .flat()
+
+          db.run(
+            `
+          INSERT OR IGNORE INTO profiles (userId, fullName, description, profileImage, sectionId)
+          VALUES ${users.map(() => '(?, ?, ?, ?, ?)').join(', ')}
+        `,
+            profileValues
+          )
+        })
       }
     )
 
-    // Включение триггера
     db.run(`
       CREATE TRIGGER IF NOT EXISTS set_default_section
       AFTER INSERT ON users
@@ -101,11 +129,4 @@ export const seedDatabase = () => {
       END;
     `)
   })
-}
-
-const createPermanentToken = (email, password) => {
-  const firstHash = CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex)
-  const combined = email + firstHash
-  const permanentToken = CryptoJS.SHA256(combined).toString(CryptoJS.enc.Hex)
-  return permanentToken
 }
