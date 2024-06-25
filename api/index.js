22b261c219ef57c42de92ebac832b2d69d52efa1
Neirris <nepkka@gmail.com>
2024-06-25 13:46:00 +0500

    fix index

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import { initializeDatabase } from '../server/data/database.js'
-import { seedDatabase } from '../server/data/seed.js'
+import { initializeDatabase } from './data/database.js'
+import { seedDatabase } from './data/seed.js'
 import authRouter from './routes/auth.js'
 import profileRouter from './routes/profile.js'
 import usersRouter from './routes/users.js'
@@ -12,6 +12,7 @@ import eventsRouter from './routes/events.js'
 import notificationsRouter from './routes/notifications.js'
 import adminRouter from './routes/admin.js'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const app = express()
@@ -21,8 +22,9 @@ app.use(bodyParser.urlencoded({ limit: '25mb', extended: true }))
 app.use(cookieParser())
 app.use(cors({ origin: process.env.API_BASE_URL, credentials: true }))
 
-initializeDatabase()
-seedDatabase()
+initializeDatabase().then(() => {
+  seedDatabase()
+})
 
 app.use('/auth', authRouter)
 app.use('/profile', profileRouter)
